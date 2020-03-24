00b5f2a539073619f752d1baf76cf239b8b4909c
Alex Levy <alex.j.levy@gmail.com>
2020-03-24 17:29:44 -0500

    first commit

---
+++ src/main/java/org/launchcode/tapIn/TapInApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.tapIn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapInApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapInApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/tapIn/TapInApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.tapIn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapInApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
