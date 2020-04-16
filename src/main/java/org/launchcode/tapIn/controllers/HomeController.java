dff1cef7117617c3ded6307b1cd3c940688a4210
Alex Levy <alex.j.levy@gmail.com>
2020-04-16 16:43:56 -0500

    added tapIn form

--- src/main/java/org/launchcode/tapIn/controllers/HomeController.java
+++ src/main/java/org/launchcode/tapIn/controllers/HomeController.java
@@ -2,10 +2,12 @@ package org.launchcode.tapIn.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("tapIn")
 public class HomeController {
 
     @GetMapping
-    public String home() { return "home"; }
+    public String tap() { return "tapIn/index"; }
 }
--- src/main/java/org/launchcode/tapIn/controllers/TapController.java
+++ src/main/java/org/launchcode/tapIn/controllers/TapController.java
@@ -1,4 +1,11 @@
 package org.launchcode.tapIn.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TapController {
+
+    @GetMapping
+    public String home() { return "home"; }
 }
