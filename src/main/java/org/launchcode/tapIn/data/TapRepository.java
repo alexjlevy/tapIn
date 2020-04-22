8141df2a6eddc31b9852cb49e46b318ad885dece
Alex Levy <alex.j.levy@gmail.com>
2020-04-22 11:17:54 -0500

    Building our Tap class + repo

---
+++ src/main/java/org/launchcode/tapIn/data/TapRepository.java
@@ -0,0 +1,4 @@
+package org.launchcode.tapIn.data;
+
+public interface TapRepository {
+}
--- src/main/java/org/launchcode/tapIn/models/Tap.java
+++ src/main/java/org/launchcode/tapIn/models/Tap.java
@@ -1,4 +1,42 @@
 package org.launchcode.tapIn.models;
 
-public class Tap {
+import com.sun.istack.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+
+@Entity
+public class Tap extends AbstractEntity {
+
+    @NotNull
+    @OneToOne
+    private LocalDate date;
+
+    @ManyToOne
+    @NotNull
+    private Goal goals;
+
+    public Tap(LocalDate date, Goal goals) {
+        this.date = LocalDate.now();
+        this.goals = goals;
+    }
+
+    public Tap() { }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Goal getGoals() {
+        return goals;
+    }
+
+    public void setGoals(Goal goals) {
+        this.goals = goals;
+    }
 }
